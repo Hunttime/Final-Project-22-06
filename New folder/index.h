160ceb20d5817228e54d254be77b195c4d454bd0
Daniel Hunt <dhunt@tarta.com>
2022-08-03 20:00:32 -0400

    project

---
+++ New folder/index.h
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+    header { 
+        height:100px;
+        background-color: gold;
+    }
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="https://google.com">google</a>
+
+                </li>
+            <li><li>
+            <li><li>
+            </ul>
+        </nav>   
+</body>
+</html>
+
